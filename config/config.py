22a1abf91d2309a5e1faba048bfa4867f38ecfae
Gleb Dovzhenko <dovjenko.g@gmail.com>
2018-06-05 18:40:34 +0200

    Added error msg for when app is locked. [3]

--- config/config.py
+++ config/config.py
@@ -5,6 +5,3 @@ project_root = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__f
 instr_path = os.path.join(project_root, 'mcstas_data', 'instruments')
 results_path = os.path.join(project_root, 'mcstas_data', 'results')
 img_path = os.path.join(project_root, 'mcstas_data', 'images')
-
-if not os.path.exists(instr_path):
-    raise FileNotFoundError('Вы не разблокировали приложение! Обратитесь к инструкции.')

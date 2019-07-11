af96c6343fe0bfacbe40a23a6f39cafd5673184b
Gleb Dovzhenko <dovjenko.g@gmail.com>
2019-07-11 08:01:43 +0200

    Removed the lock-unlock related files

--- h3_reflectometer.py
+++ h3_reflectometer.py
@@ -44,6 +44,6 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
     pinhole_sans_app = TLabAppQt(name='Reflectometer',
                                  env_config=app_config,
-                                 instr_params=instrument_params, dummy=False)
+                                 instr_params=instrument_params, dummy=True)
     pinhole_sans_app.show()
     sys.exit(app.exec_())

f07c132b9b90e5f830ce5fabda5d9c2b33579169
Gleb Dovzhenko <dovjenko.g@gmail.com>
2020-08-31 13:54:19 +0200

    dummy

--- h5_therm_diff.py
+++ h5_therm_diff.py
@@ -5,6 +5,6 @@ from PyQt5.QtWidgets import QApplication
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    pinhole_sans_app = H5ThermApp(dummy=True)
+    pinhole_sans_app = H5ThermApp(dummy=False)
     pinhole_sans_app.show()
     sys.exit(app.exec_())
--- h8_stress_diff.py
+++ h8_stress_diff.py
@@ -5,6 +5,6 @@ from PyQt5.QtWidgets import QApplication
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    pinhole_sans_app = H8StressApp(dummy=True)
+    pinhole_sans_app = H8StressApp(dummy=False)
     pinhole_sans_app.show()
     sys.exit(app.exec_())
